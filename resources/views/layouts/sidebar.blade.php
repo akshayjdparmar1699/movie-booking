ed01d53815221325604098eda8cca6f7e563d3d0
Akiran <akshayjdparmar1699@gmail.com>
2025-05-17 15:00:40 +0530

    Initial commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-dark text-white p-3" style="width: 250px; min-height: 100vh;">
+    <h4 class="mb-4">Business Demo</h4>
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item mb-2">
+            <a href="#" class="nav-link text-white">Movies</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link text-white"></a>
+        </li>
+    </ul>
+</div>
