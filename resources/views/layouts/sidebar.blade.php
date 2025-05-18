8a59b274f67b9b43e62e7ee265ddf9a4d47898da
Akiran <akshayjdparmar1699@gmail.com>
2025-05-18 16:16:09 +0530

    Title changed.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-dark text-white p-3" style="width: 250px; min-height: 100vh;">
-    <h4 class="mb-4">Business Demo</h4>
+    <h4 class="mb-4">Movie Booking</h4>
     <ul class="nav nav-pills flex-column">
         <li class="nav-item">
             <a href="{{ route('theaters.index') }}" class="nav-link text-white">Theaters</a>
@@ -7,6 +7,5 @@
         <li class="nav-item mb-2">
             <a href="{{ route('movies.index') }}" class="nav-link text-white">Movies</a>
         </li>
-
     </ul>
 </div>
